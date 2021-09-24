efa7f8d85f318a920a4073d5577882d38bf2a64b
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-24 16:12:39 -0400

    custom render method

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -1,22 +1,34 @@
 // testing with context and a custom render method
 // http://localhost:3000/easy-button
 
+import {render as rtlRender, screen} from '@testing-library/react'
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
+import {ThemeProvider} from '../../components/theme'
+
+function renderWithProviders(ui, {theme = 'light', options} = {}) {
+  const Wrapper = ({children}) => {
+    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+  }
+  return rtlRender(ui, {wrapper: Wrapper, ...options})
+}
 
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+  renderWithProviders(<EasyButton>Easy</EasyButton>)
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  renderWithProviders(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
