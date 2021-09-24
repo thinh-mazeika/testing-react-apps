81042bf4f93d8397b3bf9a606d7419b75ae189e8
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-24 11:40:17 -0400

    generate test data

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -1,27 +1,66 @@
 // form testing
 // http://localhost:3000/login
 
-import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import faker from 'faker'
+import * as React from 'react'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  let submittedData
+  const handleSubmit = data => {
+    return (submittedData = data)
+  }
+  render(<Login onSubmit={handleSubmit} />)
+  const username = screen.getByLabelText(/username/i)
+  const password = screen.getByLabelText(/password/i)
+  userEvent.type(username, 'thinh')
+  userEvent.type(password, '123')
+  const submitBtn = screen.getByRole('button', {name: /submit/i})
+  userEvent.click(submitBtn)
+
+  expect(submittedData).toEqual({username: 'thinh', password: '123'})
+})
+
+test('submitting the form calls onSubmit with username and password with jest mock fn', () => {
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+  const username = screen.getByLabelText(/username/i)
+  const password = screen.getByLabelText(/password/i)
+  userEvent.type(username, 'thinh')
+  userEvent.type(password, '123')
+  const submitBtn = screen.getByRole('button', {name: /submit/i})
+  userEvent.click(submitBtn)
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: 'thinh',
+    password: '123',
+  })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+test('submitting the form calls onSubmit with username and password with faker', () => {
+  const buildLoginForm = () => {
+    return {
+      username: faker.name.findName(),
+      password: faker.internet.password(),
+    }
+  }
+  const {username, password} = buildLoginForm()
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  const submitBtn = screen.getByRole('button', {name: /submit/i})
+  userEvent.click(submitBtn)
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: username,
+    password: password,
+  })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
 /*
