3011141733f621294fe93d9510dd897d5849edcc
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2021-09-24 16:23:50 -0400

    changes import

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -1,20 +1,13 @@
 // testing with context and a custom render method
 // http://localhost:3000/easy-button
 
-import {render as rtlRender, screen} from '@testing-library/react'
+import {screen} from '@testing-library/react'
 import * as React from 'react'
 import EasyButton from '../../components/easy-button'
-import {ThemeProvider} from '../../components/theme'
-
-function renderWithProviders(ui, {theme = 'light', options} = {}) {
-  const Wrapper = ({children}) => {
-    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-  }
-  return rtlRender(ui, {wrapper: Wrapper, ...options})
-}
+import {render} from '../../test/test-utils'
 
 test('renders with the light styles for the light theme', () => {
-  renderWithProviders(<EasyButton>Easy</EasyButton>)
+  render(<EasyButton>Easy</EasyButton>)
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -23,7 +16,7 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with the dark styles for the dark theme', () => {
-  renderWithProviders(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
